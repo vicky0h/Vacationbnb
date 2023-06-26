c2f45af3f36dc1436bdb16b7bb6bdc14c02f9954
Vicky H <63320567+vicky0h@users.noreply.github.com>
2023-06-26 09:43:14 +0800

    Update backend index.js cors path

--- api/index.js
+++ api/index.js
@@ -25,7 +25,7 @@ app.use('/uploads', express.static(__dirname + '/uploads'));
 app.use(cors({
     credentials: true,
     // origin: 'http://localhost:5173',
-    origin: 'http://vacationbnb.vercel.app',
+    origin: 'https://vacationbnb.onrender.com',
 }));
 
 
