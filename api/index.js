21615e358498759ac71f721085863e564a9e9292
Vicky H <63320567+vicky0h@users.noreply.github.com>
2023-06-26 11:23:54 +0800

    Update index.js corn path

--- api/index.js
+++ api/index.js
@@ -24,8 +24,8 @@ app.use(cookieParser());
 app.use('/uploads', express.static(__dirname + '/uploads'));
 app.use(cors({
     credentials: true,
-    origin: 'http://localhost:5173',
-    // origin: 'http://vacationbnb.vercel.app',
+    // origin: 'http://localhost:5173',
+    origin: 'http://vacationbnb.vercel.app',
 }));
 
 
