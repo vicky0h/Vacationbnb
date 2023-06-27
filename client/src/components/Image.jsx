869dad7bf55740971e7de73d46b524b705e8ba1b
Vicky H <63320567+vicky0h@users.noreply.github.com>
2023-06-27 11:40:48 +0800

    Update Image.jsx src path to render

--- client/src/components/Image.jsx
+++ client/src/components/Image.jsx
@@ -3,6 +3,6 @@ export default function Image({ src, ...rest }) {
     src && src.includes("https://")
       ? "uploads/" + src
       // : "http://localhost:4000/" + src;
-      : "https://github.com/vicky0h/Vacationbnb/tree/main/api/" + src;
+      : "https://vacationbnb.onrender.com/" + src;
   return <img {...rest} src={src} alt={""} />;
 }
