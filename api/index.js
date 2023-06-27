8b06989c982b55cd6bb87d94daff76a2ee2c9d14
Vicky H <63320567+vicky0h@users.noreply.github.com>
2023-06-27 08:36:47 +0800

    Update index.js cors origin

--- api/index.js
+++ api/index.js
@@ -27,7 +27,7 @@ app.use('/uploads', express.static(__dirname + '/uploads'));
 // }));
 app.use(cors({
   credentials: true,
-  origin: 'https://64999dd1b00a0207c4ac1504--vacationbnb.netlify.app/'
+  origin: 'https://vacationbnb.netlify.app/'
 }));
 
 
