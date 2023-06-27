c06e3e3e832cc1a6ce827f38419b28c9b3b6abfa
Vicky H <63320567+vicky0h@users.noreply.github.com>
2023-06-27 10:19:53 +0800

    Update Image.jsx localhost path

--- client/src/components/Image.jsx
+++ client/src/components/Image.jsx
@@ -2,6 +2,7 @@ export default function Image({ src, ...rest }) {
   src =
     src && src.includes("https://")
       ? "uploads/" + src
-      : "http://localhost:4000/" + src;
+      // : "http://localhost:4000/" + src;
+      : "https://github.com/vicky0h/Vacationbnb/tree/main/api/" + src;
   return <img {...rest} src={src} alt={""} />;
 }
